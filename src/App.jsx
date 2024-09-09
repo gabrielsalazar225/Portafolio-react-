a6046a5fb4ff005cd036136197cfedd40974669e
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-09-09 00:42:53 -0400

    header partialy done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-import './App.css'
+
+import Sidenav from './components/Sidenav'
+
 
 
 function App() {
 
   return (
     <>
-      
+      <Sidenav/>
     </>
   )
 }
