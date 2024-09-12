09272e134fd0f496fa22a3c935d6c130c22f9adb
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-09-12 00:44:45 -0400

    main component done, starting work and workItem components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,11 +8,15 @@ const Main = () => {
         <div id='main'>
             <img className="w-full h-screen object-cover object left scale-x-[-1] "
              src='../public/Main-bg.jpg'
-             alt='Back ground laptop image '/>
+             alt='Back ground laptop image'
+             />
             
             <div className="w-full h-screen absolute top-0 left-0 bg-white/30">
-            <h1> I'm Gabriel Salazar</h1>
-            <h2> I'm a
+            <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start  items -center'>
+             <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I am Gabriel Salazar</h1>
+              <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
+                I am a
+
             <TypeAnimation
       sequence={[
         'Developer',
@@ -28,15 +32,16 @@ const Main = () => {
       repeat={Infinity}
     />
             </h2>
-            <div>
+            <div className='flex justify-between pt-6 max-w-[200px] w-full'>
                 <FaTwitter className='cursor-pointer' size={20}/>
                 <FaFacebookF className='cursor-pointer' size={20}/>
                 <FaInstagram className='cursor-pointer' size={20}/>
                 <FaLinkedinIn className='cursor-pointer' size={20}/>
-            </div>
-            </div>
+           </div>
+          </div>
+         </div>
         </div>
-    )
-}
+    );
+};
 
 export default Main
